dd6f8dd8e4f530620576424b085c66ae22b7cfe2
yahyazeghmane <mryahyazeghmane@gmail.com>
2023-09-19 14:34:24 +0000

    first commitÃÃ

---
+++ src/main/java/com/snim/GestionStagiairesApplication.java
@@ -0,0 +1,13 @@
+package com.snim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStagiairesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStagiairesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snim/GestionStagiairesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStagiairesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
