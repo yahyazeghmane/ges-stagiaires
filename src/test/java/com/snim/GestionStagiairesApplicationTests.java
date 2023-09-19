66db86dd0956f1a5a51581b10a0e86aabe71cff9
yahyazeghmane <mryahyazeghmane@gmail.com>
2023-09-19 18:43:37 +0000

    Ãsecond commitÃ

--- src/test/java/com/snim/GestionStagiairesApplicationTests.java
+++ src/test/java/com/snim/GestionStagiairesApplicationTests.java
@@ -1,13 +1,20 @@
 package com.snim;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GestionStagiairesApplicationTests {
-
+	private static Logger logger = LoggerFactory.getLogger(GestionStagiairesApplicationTests.class);
 	@Test
 	void contextLoads() {
+		//first test:
+		logger.info("first test");
+		Assert.assertEquals(1+1, 2);
 	}
+
 
 }
